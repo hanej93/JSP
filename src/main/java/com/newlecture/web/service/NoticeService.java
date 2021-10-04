585171dd272ccaf4e3fc31e6cfff5e07524a5989
hanej93 <hanej93@naver.com>
2021-10-04 19:29:50 +0900

    1. 게시판 페이지 조회를 위한 쿼리문 작성   - row_number() 함수 활용: 일정한 일련번호를 위함      -> @rownum, row_number() over 두가지 방식

--- src/main/java/com/newlecture/web/service/NoticeService.java
+++ src/main/java/com/newlecture/web/service/NoticeService.java
@@ -16,13 +16,27 @@ public class NoticeService {
 	}
 
 	public List<Notice> getNoticeList(String field, String query, int page) {
+		
+//		String sql = "select * from ("
+//					+ "	select @rownum := @rownum+1 as num,"
+//					+ "	N1.* FROM "
+//					+ "	(select * from notice order by regdate desc) N1,"
+//					+ "	(select @rownum := 0) r "
+//					+ ") N2"
+//					+ "where num between 6 and 10";
 
+		String sql = "select * from("
+					+ "		select row_number() over (order by regdate desc) num,"
+					+ "		notice.* from notice"
+					+ "	) N "
+					+ "where num between 6 and 10";
+		
 		return null;
 	}
 
 	public int getNoticeCount() {
 
-		return 0;
+		return getNoticeCount("title", "");
 	}
 
 	public int getNoticeCount(String field, String query) {
